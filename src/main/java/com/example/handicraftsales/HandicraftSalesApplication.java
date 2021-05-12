1d562fa180549e2250de1fc07b946f1c056aa8a0
Barış Önen <bonen@etu.edu.tr>
2021-05-12 21:20:39 +0300

    Fixes on 404 issue. Starting to construct project structure.

--- src/main/java/com/example/handicraftsales/HandicraftSalesApplication.java
+++ src/main/java/com/example/handicraftsales/HandicraftSalesApplication.java
@@ -1,50 +1,14 @@
 package com.example.handicraftsales;
 
-import com.example.handicraftsales.entities.Customer;
-import com.example.handicraftsales.entities.Product;
-import com.example.handicraftsales.repositories.CustomerRepository;
-import com.example.handicraftsales.repositories.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 
 @SpringBootApplication
-public class HandicraftSalesApplication implements CommandLineRunner {
-
-	private final MongoTemplate mongoTemplate;
-
-	@Autowired
-	public HandicraftSalesApplication(MongoTemplate mongoTemplate) {
-		this.mongoTemplate = mongoTemplate;
-	}
-
-	@Autowired
-	private CustomerRepository customerRepository;
-
-	@Autowired
-	private ProductRepository productRepository;
+public class HandicraftSalesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HandicraftSalesApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		System.out.println();
-		for(Customer customer : customerRepository.findAll()) {
-			System.out.println(customer);
-		}
-		System.out.println("---");
-
-		for(Product product : productRepository.findAll()) {
-			System.out.println(product);
-		}
-		System.out.println();
-
 	}
 
 }
