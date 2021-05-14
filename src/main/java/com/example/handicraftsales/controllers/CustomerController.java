6dc47d8387349ab10328a79dcd90613bed2e3862
Barış Önen <bonen@etu.edu.tr>
2021-05-14 22:31:27 +0300

    New request handlers added for Product and Customer Controllers.

--- src/main/java/com/example/handicraftsales/controllers/CustomerController.java
+++ src/main/java/com/example/handicraftsales/controllers/CustomerController.java
@@ -2,12 +2,14 @@ package com.example.handicraftsales.controllers;
 
 import com.example.handicraftsales.entities.Customer;
 import com.example.handicraftsales.repositories.CustomerRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
@@ -18,10 +20,45 @@ public class CustomerController {
     private CustomerRepository customerRepository;
 
     @GetMapping
-    public List<Customer> getAllProducts() {
+    public List<Customer> getAllCustomers() {
 
-        return customerRepository.findAll();
+        List<Customer> currentData = customerRepository.findAll();
+        if(currentData == null)
+            return new ArrayList<>();
+        return currentData;
     }
+
+    @PatchMapping
+    public HttpStatus createDefaultData() {
+        List<Customer> currentData = getAllCustomers();
+        if(currentData.isEmpty()) {
+            List<Customer> newData = new ArrayList<>();
+            newData.add(new Customer("baris onen", "1234", "barisonen.1996@gmail.com"));
+            newData.add(new Customer("burhan altintop", "qwerty","burhan_altintop@gmail.com"));
+            customerRepository.saveAll(newData);
+            return HttpStatus.OK;
+        }
+        return HttpStatus.NOT_MODIFIED;
+    }
+
+    @DeleteMapping("/deleteAll")
+    public HttpStatus deleteAll() {
+        customerRepository.deleteAll();
+        return HttpStatus.OK;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public HttpStatus deleteById(@PathVariable String id) {
+        Optional<Customer> c = customerRepository.findById(id);
+        if(c.isPresent()) {
+            customerRepository.deleteById(id);
+            return HttpStatus.OK;
+        }
+        else {
+            return HttpStatus.NOT_MODIFIED;
+        }
+    }
+
 
 
 }
--- src/main/java/com/example/handicraftsales/controllers/ProductController.java
+++ src/main/java/com/example/handicraftsales/controllers/ProductController.java
@@ -1,10 +1,15 @@
 package com.example.handicraftsales.controllers;
 
+import com.example.handicraftsales.entities.Customer;
 import com.example.handicraftsales.entities.Product;
 import com.example.handicraftsales.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
@@ -20,5 +25,34 @@ public class ProductController {
         return productRepository.findAll();
     }
 
+    @PatchMapping
+    public HttpStatus createDefaultData() {
+        List<Product> currentData = getAllProducts();
+        if(currentData.isEmpty()) {
+            List<Product> newData = new ArrayList<>();
+            newData.add(new Product("Kolye", 100, 10));
+            newData.add(new Product("Yüzük", 150,5));
+            productRepository.saveAll(newData);
+            return HttpStatus.OK;
+        }
+        return HttpStatus.NOT_MODIFIED;
+    }
 
+    @DeleteMapping("/deleteAll")
+    public HttpStatus deleteAll() {
+        productRepository.deleteAll();
+        return HttpStatus.OK;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public HttpStatus deleteById(@PathVariable String id) {
+        Optional<Product> c = productRepository.findById(id);
+        if(c.isPresent()) {
+            productRepository.deleteById(id);
+            return HttpStatus.OK;
+        }
+        else {
+            return HttpStatus.NOT_MODIFIED;
+        }
+    }
 }
