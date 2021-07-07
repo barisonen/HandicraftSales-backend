8faf537279d7379ee5b814bff5c45abd5247e308
Barış Önen <bonen@etu.edu.tr>
2021-07-07 22:33:10 +0300

    login controller added

---
+++ src/main/java/com/example/handicraftsales/controllers/LoginController.java
@@ -0,0 +1,19 @@
+package com.example.handicraftsales.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@RestController
+@RequestMapping("/customers")
+
+public class LoginController {
+
+    @PostMapping("/login")
+    public HttpStatus login(@RequestBody String username, @RequestBody String password) {
+        if("admin@example.com".equals(username) && "1234".equals(password)) {
+            return HttpStatus.OK;
+        }
+        else return HttpStatus.UNAUTHORIZED;
+    }
+}
