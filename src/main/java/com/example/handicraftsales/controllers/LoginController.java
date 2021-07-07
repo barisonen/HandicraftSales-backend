721eb3a9478747e09406187281dede4d9e14464a
Barış Önen <bonen@etu.edu.tr>
2021-07-08 00:19:11 +0300

    Login controller updated

--- src/main/java/com/example/handicraftsales/controllers/LoginController.java
+++ src/main/java/com/example/handicraftsales/controllers/LoginController.java
@@ -1,5 +1,10 @@
 package com.example.handicraftsales.controllers;
-
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import jdk.nashorn.internal.ir.ObjectNode;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,9 +14,16 @@ import org.springframework.web.bind.annotation.*;
 
 public class LoginController {
 
+    private static JSONObject jObject = null;
+    String email = "";
+    String password = "";
+
     @PostMapping
-    public HttpStatus login(@RequestBody String username, @RequestBody String password) {
-        if("admin@example.com".equals(username) && "1234".equals(password)) {
+    public HttpStatus login(@RequestBody String object) throws JSONException {
+        jObject = new JSONObject(object);
+        email = jObject.getString("email");
+        password = jObject.getString("password");
+        if("admin@example.com".equals(email) && "1234".equals(password)) {
             return HttpStatus.OK;
         }
         else return HttpStatus.UNAUTHORIZED;
